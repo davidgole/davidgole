c33c1c0f6f52fd9be1381cc45760742685e33074
davidgole1 <david.gole1@student.um.si>
2025-02-17 23:54:15 +0100

    osnovni projekt

---
+++ src/app/about/page.jsx
@@ -0,0 +1,11 @@
+// src/app/about/page.js
+export default function About() {
+    return (
+        <div className="bg-gray-100 min-h-screen p-8">
+            <h1 className="text-3xl font-bold text-blue-600 mb-4">O meni</h1>
+            <p className="text-gray-700">
+                Tukaj je nekaj informacij o meni. Uporabljam Tailwind CSS za stilizacijo!
+            </p>
+        </div>
+    );
+}
