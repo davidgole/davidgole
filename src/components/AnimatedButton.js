65e7747f9b10146cfa4808970dc0b003acb37f26
davidgole1 <david.gole1@student.um.si>
2025-04-01 19:44:21 +0200

    spremenjen gumb in sprememba navigacije

--- src/components/AnimatedButton.js
+++ src/components/AnimatedButton.js
@@ -2,8 +2,17 @@
 import { motion } from "framer-motion";
 
 export default function AnimatedButton({ children }) {
+
+    const scrollToContact = () => {
+        const contactSection = document.getElementById("contact");
+        if (contactSection) {
+            contactSection.scrollIntoView({ behavior: "smooth" });
+        }
+    };
+
     return (
         <motion.button
+            onClick={scrollToContact}
             className="relative mt-5 px-6 py-3 border rounded-lg text-[var(--foreground)] font-semibold
                        transition-all overflow-hidden bg-[var(--background)]"
             initial={{ boxShadow: "0px 0px 0px rgba(255, 102, 0, 0)" }}
@@ -11,7 +20,7 @@ export default function AnimatedButton({ children }) {
                 boxShadow: "0px 0px 20px rgba(255, 102, 0, 0.8)", // Takojšnja močnejša senca
             }}
             whileTap={{
-                scale:0.9,
+                scale: 0.9,
             }}
             transition={{
                 duration: 0.2, // Hitrejša animacija za boljši odziv
